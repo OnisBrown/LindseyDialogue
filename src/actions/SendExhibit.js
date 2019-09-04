59d54f8e1b0d36c03bae2691dd43309fe72b94f8
OnisBrown <onis.Brown@hotmail.co.uk>
2019-09-04 11:13:19 +0100

    hurrdurr

---
+++ src/actions/SendExhibit.js
@@ -0,0 +1,16 @@
+/**
+ * @title send exhibit to
+ * @category LindseyDialogue
+ * @author OnisBrown.
+ */
+/** const virtual_machine = (bp: SDK, user, session, temp, bot, event, args) => { */
+const axios
+var msg = session.slots.Exhibits.value;
+
+
+
+
+
+
+
+/** } */
