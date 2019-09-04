76b662929aa70390bf2716adb5c7b0d50a048ea7
OnisBrown <onis.Brown@hotmail.co.uk>
2019-09-04 17:16:10 +0100

    Yarn start bug

--- src/actions/SendExhibit.js
+++ src/actions/SendExhibit.js
@@ -4,11 +4,20 @@
  * @author OnisBrown.
  */
 /** const virtual_machine = (bp: SDK, user, session, temp, bot, event, args) => { */
-const axios
-var msg = session.slots.Exhibits.value;
 
+const axios = require('axios');
+var msg = args;
 
+const callApi = async () => {
+  // We call the Github API
+  const { data } = await axios.get('https://api.github.com/orgs/botpress/repos')
 
+  // We assign the response to the session variable so we can use it later
+  session.response = data
+}
+
+// Actions are async, so make sure to return a promise
+return callApi()
 
 
 
